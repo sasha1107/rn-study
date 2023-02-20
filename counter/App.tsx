892ced352316ac94c920b54bd34345ae751fd5ec
Soohyun Jung <sasha1107@naver.com>
2023-02-20 23:35:43 +0900

    Configuring the navigator
    - https://reactnavigation.org/docs/hello-react-navigation\#configuring-the-navigator
    - initialRouteName => 초기 렌더링 화면

--- counter/App.tsx
+++ counter/App.tsx
@@ -12,14 +12,22 @@ function HomeScreen() {
     </View>
   );
 }
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
