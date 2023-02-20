4220d9f5ab6457d0ff7095d9c60027f11cf90dda
Soohyun Jung <sasha1107@naver.com>
2023-02-20 22:01:08 +0900

    Feat: useState 사용해보기

--- counter/App.tsx
+++ counter/App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [cnt, setCnt] = useState<number>(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.assa}>Open up App.tsx to start working on your app!우하하</Text>
+      <Text style={styles.cnt}>{cnt}</Text>
+      <Button title="더하기" onPress={() => setCnt(cnt + 1)}></Button>
+      <Button title="빼기" onPress={() => setCnt(cnt - 1)}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +23,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  assa: {
+    color: 'pink',
+  },
+  cnt: {
+    fontSize: 50,
+    marginTop: 20,
+  }
 });
