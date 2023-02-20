dff3584caa1948d22535e6bc44138236afdc19ed
Soohyun Jung <sasha1107@naver.com>
2023-02-20 23:29:12 +0900

    React Navigation 설치
    - https://reactnavigation.org/docs/hello-react-navigation\#creating-a-native-stack-navigator
    - Createing a native stack navigator

--- counter/App.tsx
+++ counter/App.tsx
@@ -1,59 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-  const [lotto, setLotto] = useState<number[]>([]);
-
-  function makeLotto() {
-    let lnumber:number[] = [];
-    let rnumber:number = 0;
-
-    let lottos = [];
-    let a  = 0;
-    for (let i = 1; i <= 45; i++) {
-      lottos.push(i);
-    }
-    for (let i = 0; i < 45; i++) {
-      rnumber = ~~(Math.random() * 45);
-      a = lottos[i];
-      lottos[i] = lottos[rnumber];
-      lottos[rnumber] = a;
-    }
-    console.log(lottos);
-    for (let i = 0; i < 6; i++) {
-      lnumber.push(lottos[i]);
-    }
-    setLotto(lnumber);
-  }
-
-  useEffect(() => {
-    makeLotto()  
-  }, [])
-
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={{marginBottom:100,}}>로또 앱</Text>
-      <Button title='로또 번호 생성' onPress={()=> makeLotto()}></Button>
-      <Text style={{fontSize: 25,}}>{lotto.toLocaleString()}</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  assa: {
-    color: 'salmon',
-  },
-  cnt: {
-    fontSize: 50,
-    marginTop: 20,
-  }
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
